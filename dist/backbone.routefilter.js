d81d076dbd1aaf3f6cd105ccf3425e5437cc3c49
Boaz Sender <boaz@bocoup.com>
2012-08-29 02:32:25 -0400

    adds dist directory and built files

---
+++ dist/backbone.routefilter.js
@@ -0,0 +1,27 @@
+/*! backbone.routefilter - v0.1-pre - 2012-08-29
+* undefined
+* Copyright (c) 2012 Boaz Sender; Licensed  */
+
+(function(Backbone, _) {
+
+  var nop = function(){};
+
+  _.extend(Backbone.Router.prototype, {
+    before: nop,
+    after: nop,
+    route: function(route, name, callback) {
+      this.before(name);
+      Backbone.history || (Backbone.history = new Backbone.History);
+      if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+      if (!callback) callback = this[name];
+      Backbone.history.route(route, _.bind(function(fragment) {
+        var args = this._extractParameters(route, fragment);
+        callback && callback.apply(this, args);
+        this.trigger.apply(this, ['route:' + name].concat(args));
+        Backbone.history.trigger('route', this, name, args);
+      }, this));
+      this.after(name);
+      return this;
+    }
+  });
+}(Backbone, _));
