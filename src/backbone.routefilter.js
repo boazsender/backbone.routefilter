49632ff4b7c21333f3e33a856f9dee64b18e9d98
Boaz Sender <boaz@bocoup.com>
2012-08-28 23:07:27 -0400

    staring project from the grunt jQuery template + backbone and underscore

---
+++ src/backbone.routefilter.js
@@ -0,0 +1,11 @@
+/*
+ * backbone.routefilter
+ * https://github.com/boazsender/backbone.routefilter
+ *
+ * Copyright (c) 2012 Boaz Sender
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function(Backbone, _) {
+
+}(Backbone, _));
