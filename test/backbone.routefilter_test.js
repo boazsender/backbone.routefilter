33b4ef365fa730409a574c42858450fe6cafc591
Boaz Sender <boaz@bocoup.com>
2012-08-29 21:04:08 -0400

    adds 3 more unit tests to exercise return false behaviour for before filter

--- test/backbone.routefilter_test.js
+++ test/backbone.routefilter_test.js
@@ -1,7 +1,7 @@
 /*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
 /*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
-/*global Backbone:false, _: false*/
+/*global Backbone:false, _: false, console: false*/
 (function($, Backbone, _) {
   /*
     ======== A Handy Little QUnit Reference ========
@@ -21,65 +21,126 @@
       notStrictEqual(actual, expected, [message])
       raises(block, [expected], [message])
   */
+
+  var harness = window.harness = {};
+
   module("routes", {
     setup: function() {
-      var self = this;
-
+      
       // Set up a cache to store test data in
-      self.cache = window.cache= {};
+      harness.cache = {};
 
       // Set up a test router
-      self.Router = Backbone.Router.extend({
+      harness.Router = Backbone.Router.extend({
         routes: {
           "": "index",
           "page/:id": "page"
         },
         before: function( route ) {
-          self.cache.before = (route||true);
+          harness.cache.before = (route||true);
         },
         after: function( route ) {
-          self.cache.after = (route||true);
+          harness.cache.after = (route||true);
         },
         index: function( route ){
-          self.cache.route = "";
+          harness.cache.route = "";
         },
         page: function( route ){
-          self.cache.route = route;
+          harness.cache.route = route;
         }        
       });
 
-      self.router = new self.Router();
+      harness.router = new harness.Router();
+      Backbone.history.start();
+    },
+    teardown: function() {
+      harness.router.navigate("", false);
+      Backbone.history.stop();
     }
   });
 
   // Ensure the basic navigation still works like normal routers
-  test("basic navigation still works", function() {
-    expect(2);
-
-    var self = this;
-
+  test("basic navigation still works", 2, function() {
+ 
     // Trigger the router
-    Backbone.history.start();
-    self.router.navigate('', true);
-    equal(self.cache.route, "", "Index route triggered");
+    harness.router.navigate('', true);
+    equal(harness.cache.route, "", "Index route triggered");
     
-    self.router.navigate('page/2', true);
-    equal(self.cache.route, 2, "successfully routed to page/2, and recieved route arg of 2");
+    harness.router.navigate('page/2', true);
+    equal(harness.cache.route, 2, "successfully routed to page/2, and recieved route arg of 2");
+ 
   });
 
   // Ensure the basic navigation still works like normal routers
-  test("before and after filters work", function() {
-    expect(4);
+  test("before and after filters work", 4, function() {
+ 
+    harness.router.navigate('', true);
 
-    var self = this;
-    self.router.navigate('', true);
+    ok(harness.cache.before, "before triggered");
+    ok(harness.cache.after, "after triggered");
 
-    ok(self.cache.before, "before triggered");
-    ok(self.cache.after, "after triggered");
+    harness.router.navigate('page/2', true);
+    equal(harness.cache.before, 2, "successfully passed `2` to before filtrer after routing to page/2"); 
+    equal(harness.cache.after, 2, "successfully passed `2` to after filtrer after routing to page/2"); 
 
-    self.router.navigate('page/2', true);
-    equal(self.cache.before, 2, "successfully passed `2` to before filtrer after routing to page/2"); 
-    equal(self.cache.after, 2, "successfully passed `2` to after filtrer after routing to page/2"); 
+  });
+
+
+  module("returning from before filter", {});
+
+  // Test that return false behaves properly from inside the before filter.
+  test("return false works on before filter", 3, function() {
+    // Set up a cache to store test data in
+    var cache = {};
+
+
+    // Set up a a Router.
+    var Router = Backbone.Router.extend({
+      routes: {
+        "": "index",
+        "page/:id": "page"
+      },
+      before: function( route ) {
+        cache.before = (route || true);
+      },
+      after: function( route ) {
+        cache.after = (route||true);
+      },
+      index: function( route ){
+        cache.route = "";
+      },
+      page: function( route ){
+        cache.route = route;
+      }        
+    });
+
+    // Instantiate the Router.
+    var router = new Router();
+
+    // Start the history.
+    Backbone.history.start();
+
+    // Navigate to page two.
+    router.navigate('page/foo', true);
+
+    // Override the before filter on the fly
+    router.before = function( route ) {
+      cache.before = (route || true);
+
+      if( route === 'bar' ){
+        return false;
+      }
+    };
+   
+    // Navigate to the place our before filter is handling.
+    router.navigate('page/bar', true);
+
+    equal(cache.before, "bar", "The before filter was called, and was passed the correct arg, bar.");
+    equal(cache.after, "foo", "The orginal route callback was not called after the before filter was over ridden to return false.");
+    equal(cache.after, "foo", "The after filter was not called after the before filter was over ridden to return false");
+    
+    console.log(cache);
+
   });
 
 }(jQuery, Backbone, _));
