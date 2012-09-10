d41597082311cf6c43d7ce75b85b7350c14be540
Boaz Sender <boaz@bocoup.com>
2012-09-10 18:12:43 -0400

    Rebuilding code, Resolves issue #1. Resolves issue #2.

--- dist/backbone.routefilter.js
+++ dist/backbone.routefilter.js
@@ -9,9 +9,9 @@
 
 (function(Backbone, _) {
 
-  // Save a reference to the original _bindRoutes to be called
+  // Save a reference to the original route method to be called
   // after we pave it over.
-  var _bindRoutes = Backbone.Router.prototype._bindRoutes;
+  var originalRoute = Backbone.Router.prototype.route;
 
   // Create a reusable no operation func for the case where a before
   // or after filter is not set. Backbone or Underscore should have
@@ -28,76 +28,54 @@
     // Add default after filter.
     after: nop,
 
-    // Pave over Backbone.Router.prototype._bindRoutes.
-    _bindRoutes: function() {
+    // Pave over Backbone.Router.prototype.route, the public method used
+    // for adding routes to a router instance on the fly, and the
+    // method which backbone uses internally for binding routes to handlers
+    // on the Backbone.history singleton once it's instantiated.
+    route: function(route, name, callback) {
 
-      // Iterate over each route in this Router instance
-      _.each( this.routes, function( method, route ){
+      // If there is no callback present for this route, then set it to
+      // be the name that was set in the routes property of the constructor,
+      // or the name arguement of the route method invocation. This is what
+      // Backbone.Router.route already does. We need to do it again,
+      // because we are about to wrap the callback in a function that calls
+      // the before and after filters as well as the original callback that
+      // was passed in.
+      if( !callback ){
+        callback = this[ name ];
+      }
 
-        // If there is no callback present for this route, then don't try to
-        // wrap it, because doing so would fail.
-        if( !this[ method] ) {
+      // Create a new callback to replace the original callback that calls
+      // the before and after filters as well as the original callback
+      // internally.
+      var wrappedCallback = _.bind( function() {
+
+        // Call the before filter and if it returns false, run the
+        // route's original callback, and after filter. This allows
+        // the user to return false from within the before filter
+        // to prevent the original route callback and after
+        // filter from running.
+        if ( this.before.apply(this, arguments) === false) {
           return;
         }
 
-        // Check if we have already wrapped this handler,
-        // if not, then proceed.
-        if( !this[ method ].wrapped ){
-
-          // Do what Backbone.Router.route does and make sure the route is a
-          // RegExp. We need to mimic Backbone.Router.route internal behavior
-          // here in order to prepare the route args to be passed into our
-          // filters, and the original callback properly.
-          if (!_.isRegExp(route)) {
-            route = this._routeToRegExp(route);
-          }
-
-          // Cache the original callback so we can pave over it with
-          // a wrapper function that will call the original callback
-          // internally along with the before and after filters.
-          var originalCallback = this[ method ];
-
-          // Pave over the original callback for this route.
-          this[ method ] = function(){
-            // Grab the current url fragment from Backbone.history. We have to
-            // wait until we're inside of the route callback to try to access
-            // The Backbone.history singleton to ensure that it has been
-            // instantiated.
-            var fragment = Backbone.history.getFragment();
-
-            // Finish preparing the args now that we have the fragment.
-            var args = this._extractParameters( route, fragment );
-
-            // Call the before filter and if it returns false, run the
-            // route's original callback, and after filter. This allows
-            // the user to return false from within the before filter
-            // to prevent the after original route callback and after
-            // filter from running.
-            if( this.before.apply(this, args) !== false ){
-
-              // Call the original callback.
-              originalCallback.apply(this, args);
-
-              // Call the after filter.
-              this.after.apply(this, args);
-
-            }
-
-          };
-
-          // Set a flag on the handler so that we can check for it
-          // and not rewrap it if it's already been wrapped.
-          this[ method ].wrapped = true;
-
+        // If the callback exists, then call it. This means that the before
+        // and after filters will be called whether or not an actual
+        // callback function is supplied to handle a given route.
+        if( callback ) {
+          callback.apply( this, arguments );
         }
+
+        // Call the after filter.
+        this.after.apply( this, arguments );
 
       }, this);
 
-      // Call the original _bindRoutes function to continue Backbone's
-      // behavior of getting all the route callbacks and names
-      // onto Backbone.History.handlers.
-      _bindRoutes.apply(this, arguments);
-
+      // Call our original route, replacing the callback that was originally
+      // passed in when Backboun.Router.route was invoked with our wrapped
+      // callback that calls the before and after callbacks as well as the
+      // original callback.
+      return originalRoute.call( this, route, name, wrappedCallback );
     }
 
   });
