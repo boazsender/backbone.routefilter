028853a02d055de3c250f28009b4f2b3eecbb89e
Boaz Sender <boaz@bocoup.com>
2012-09-10 10:45:00 -0400

    adds new test for double route handler binding per issue #1

--- test/backbone.routefilter_test.js
+++ test/backbone.routefilter_test.js
@@ -26,7 +26,7 @@
 
   module("routes", {
     setup: function() {
-      
+
       // Set up a cache to store test data in
       harness.cache = {};
 
@@ -47,7 +47,7 @@
         },
         page: function( route ){
           harness.cache.route = route;
-        }        
+        }
       });
 
       harness.router = new harness.Router();
@@ -61,85 +61,146 @@
 
   // Ensure the basic navigation still works like normal routers
   test("basic navigation still works", 2, function() {
- 
+
     // Trigger the router
     harness.router.navigate('', true);
     equal(harness.cache.route, "", "Index route triggered");
-    
+
     harness.router.navigate('page/2', true);
     equal(harness.cache.route, 2, "successfully routed to page/2, and recieved route arg of 2");
- 
+
   });
 
   // Ensure the basic navigation still works like normal routers
   test("before and after filters work", 4, function() {
- 
+
     harness.router.navigate('', true);
 
     ok(harness.cache.before, "before triggered");
     ok(harness.cache.after, "after triggered");
 
     harness.router.navigate('page/2', true);
-    equal(harness.cache.before, 2, "successfully passed `2` to before filtrer after routing to page/2"); 
-    equal(harness.cache.after, 2, "successfully passed `2` to after filtrer after routing to page/2"); 
+    equal(harness.cache.before, 2, "successfully passed `2` to before filtrer after routing to page/2");
+    equal(harness.cache.after, 2, "successfully passed `2` to after filtrer after routing to page/2");
 
   });
 
 
-  module("returning from before filter", {});
+  module("returning from before filter", {
+    setup: function() {
+
+      // Set up a cache to store test data in
+      harness.cache = {};
+
+
+      // Set up a a Router.
+      harness.Router = Backbone.Router.extend({
+        routes: {
+          "": "index",
+          "page/:id": "page"
+        },
+        before: function( route ) {
+          harness.cache.before = (route || true);
+        },
+        after: function( route ) {
+          harness.cache.after = (route||true);
+        },
+        index: function( route ){
+          harness.cache.route = "";
+        },
+        page: function( route ){
+          harness.cache.route = route;
+        }
+      });
+
+      // Instantiate the Router.
+      harness.router = new harness.Router();
+
+      // Start the history.
+      Backbone.history.start();
+      harness.router.navigate("", true);
+
+    },
+    teardown: function() {
+      harness.router.navigate("", false);
+      Backbone.history.stop();
+    }
+  });
 
   // Test that return false behaves properly from inside the before filter.
   test("return false works on before filter", 3, function() {
-    // Set up a cache to store test data in
-    var cache = {};
-
-
-    // Set up a a Router.
-    var Router = Backbone.Router.extend({
-      routes: {
-        "": "index",
-        "page/:id": "page"
-      },
-      before: function( route ) {
-        cache.before = (route || true);
-      },
-      after: function( route ) {
-        cache.after = (route||true);
-      },
-      index: function( route ){
-        cache.route = "";
-      },
-      page: function( route ){
-        cache.route = route;
-      }        
-    });
-
-    // Instantiate the Router.
-    var router = new Router();
-
-    // Start the history.
-    Backbone.history.start();
 
     // Navigate to page two.
-    router.navigate('page/foo', true);
+    harness.router.navigate('page/foo', true);
 
     // Override the before filter on the fly
-    router.before = function( route ) {
-      cache.before = (route || true);
+    harness.router.before = function( route ) {
+      harness.cache.before = (route || true);
 
       if( route === 'bar' ){
         return false;
       }
     };
-   
-    // Navigate to the place our before filter is handling.
-    router.navigate('page/bar', true);
 
-    equal(cache.before, "bar", "The before filter was called, and was passed the correct arg, bar.");
-    equal(cache.after, "foo", "The orginal route callback was not called after the before filter was over ridden to return false.");
-    equal(cache.after, "foo", "The after filter was not called after the before filter was over ridden to return false");
-    
-    console.log(cache);
+    // Navigate to the place our before filter is handling.
+    harness.router.navigate('page/bar', true);
+
+    equal(harness.cache.before, "bar", "The before filter was called, and was passed the correct arg, bar.");
+    equal(harness.cache.after, "foo", "The orginal route callback was not called after the before filter was over ridden to return false.");
+    equal(harness.cache.after, "foo", "The after filter was not called after the before filter was over ridden to return false");
+
+  });
+
+
+  module("Binding multiple routes to the same handler", {
+    setup: function() {
+
+      // Set up a cache to store test data in
+      harness.cache = {};
+
+
+      // Set up a a Router.
+      harness.Router = Backbone.Router.extend({
+        routes: {
+          "": "index",
+          "foo/:id": "page",
+          "page/:id": "page"
+        },
+        before: function( route ) {
+          harness.cache.before = (route || true);
+        },
+        after: function( route ) {
+          harness.cache.after = (route||true);
+        },
+        index: function( route ){
+          harness.cache.route = "";
+        },
+        page: function( route ){
+          harness.cache.route = route;
+        }
+      });
+
+      // Instantiate the Router.
+      harness.router = new harness.Router();
+
+      // Start the history.
+      Backbone.history.start();
+      harness.router.navigate("", true);
+
+    },
+    teardown: function() {
+      harness.router.navigate("", false);
+      Backbone.history.stop();
+    }
+  });
+
+  // Test that return false behaves properly from inside the before filter.
+  test("Navigate to one of the double bound handlers", 1, function() {
+
+    // Navigate to the place our before filter is handling.
+    harness.router.navigate('foo/2', true);
+
+    equal(harness.cache.route, 2, "successfully routed to the double bound route, and it equaled the right thing");
 
   });
 
